ecb263271c9f611cf6c96b3b6b63383d283a8468
jessicasewe <jessicasewe89@gmail.com>
2024-04-30 14:56:18 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,3 @@
-//creata a name that will be xecuted through command line
-//it should display Welcome to Holberton School, what is your name?
-//the program should display Your name is: INPUT
-//when user ends, display This important software is now closing
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
@@ -12,4 +7,3 @@ process.stdin.on('data', (data) => {
 process.stdin.on('end', () => {
 	process.stdout.write('This important software is now closing\n');
 });
-
