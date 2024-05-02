e33b3eafe881243589fbef7ed4f4a6f5292d93a7
jessicasewe <jessicasewe89@gmail.com>
2024-05-02 21:10:36 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+//Create a program named 1-stdin.js that will be executed through command line:
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
